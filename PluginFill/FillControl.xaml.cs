1e6125b8a6767905fe9d43638d598f59f2c54ddc
pnikrat <sniffoza@gmail.com>
2017-01-08 14:39:21 +0100

    First plugin as user control, can import button. No functions yet

---
+++ PluginFill/FillControl.xaml.cs
@@ -0,0 +1,31 @@
+﻿using InternalShared;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PluginFill
+{
+    /// <summary>
+    /// Interaction logic for FillControl.xaml
+    /// </summary>
+    [Export(typeof(IPlugin))]
+    public partial class FillControl : UserControl, IPlugin
+    {
+        public FillControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
