ecd2a84358161b019a141a7abfb0044700c282f3
pnikrat <sniffoza@gmail.com>
2017-01-10 22:13:50 +0100

    Modified helper script for second plugin .dll

--- copy.bat
+++ copy.bat
@@ -2,5 +2,8 @@
 SET OriginPath="%~dp0\PluginResize\bin\Debug\PluginResize.dll"
 SET DestinPath="%~dp0\Dynamic Paint\bin\Debug\plugins\PluginResize.dll"
 
+SET OriginPathSecond="%~dp0\PluginImage\bin\Debug\PluginImage.dll"
+SET DestinPathSecond="%~dp0\Dynamic Paint\bin\Debug\plugins\PluginImage.dll"
 COPY %OriginPath% %DestinPath% /Y
+COPY %OriginPathSecond% %DestinPathSecond% /Y
 pause
