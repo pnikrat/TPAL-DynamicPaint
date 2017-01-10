305e1dd687d614ab79d4de72eb1429a740785015
pnikrat <sniffoza@gmail.com>
2017-01-10 18:37:40 +0100

    Make plugin's resourceDict singleton

--- PluginResize/ResourceDictionaryResize.xaml.cs
+++ PluginResize/ResourceDictionaryResize.xaml.cs
@@ -11,6 +11,13 @@ namespace PluginResize
     [Export(typeof(ResourceDictionary))]
     public partial class ResourceDictionaryResize : ResourceDictionary
     {
+        private static readonly ResourceDictionaryResize _instance = new ResourceDictionaryResize();
+
+        public static ResourceDictionaryResize Instance
+        {
+            get { return _instance; }
+        }
+
         public ResourceDictionaryResize()
         {
             InitializeComponent();
