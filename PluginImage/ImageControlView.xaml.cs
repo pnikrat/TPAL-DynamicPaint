16284623a2872170ad3e13d688e6df3568bff809
pnikrat <sniffoza@gmail.com>
2017-01-10 21:48:14 +0100

    Second plugin skeleton. Visible in main window

---
+++ PluginImage/ImageControlView.xaml.cs
@@ -0,0 +1,48 @@
+﻿using InternalShared;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PluginImage
+{
+    /// <summary>
+    /// Interaction logic for ImageControlView.xaml
+    /// </summary>     
+    [Export(typeof(IPlugin))]
+    [ExportMetadata("Name", "ImagePlugin")]
+    public partial class ImageControlView : UserControl, IPlugin
+    {
+        public ImageControlView()
+        {
+            InitializeComponent();
+        }
+
+        public void AcceptHostInterface(IHost hostInterface)
+        {
+            var vm = this.DataContext as ImageControlViewModel;
+            vm.hostInterface = hostInterface;
+        }
+
+        public void ChangeLanguagePlugin(object chosenCulture)
+        {
+            throw new NotImplementedException();
+        }
+
+        public FrameworkElement GetPluginView()
+        {
+            return this as FrameworkElement;
+        }
+    }
+}
---
+++ PluginImage/ImageControlViewModel.cs
@@ -0,0 +1,14 @@
+﻿using InternalShared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PluginImage
+{
+    public class ImageControlViewModel : ViewModelBase
+    {
+        public IHost hostInterface { get; set; }
+    }
+}
